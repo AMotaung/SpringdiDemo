f3ae43f61b53f9eb8dbfcb3f34ff54a31088ffb6
AMotuang <alfredthabang@gmail.com>
2021-01-21 09:28:29 +0200

    added all files

---
+++ src/main/java/com/springdi/services/ShippingService.java
@@ -0,0 +1,8 @@
+package com.springdi.services;
+
+public class ShippingService {
+	
+	public void initShipping() {
+		System.out.println("Initializing...shipping process...");
+	}
+}
---
+++ src/main/java/com/springdi/services/TopicService.java
@@ -0,0 +1,20 @@
+package com.springdi.services;
+
+import java.util.List;
+
+import java.util.Set;
+
+public class TopicService {
+	Set<String> topics;
+
+	public Set<String> getTopics() {
+		return topics;
+	}
+
+	public void setTopics(Set<String> topics) {
+		this.topics = topics;
+	}
+	
+	
+
+}
